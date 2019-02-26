ff494b3cdfec6348df8b0e53eeaa3a4f7030e332
lamborqq <471127079@qq.com>
2019-02-26 23:14:14 +0800

    home page

---
+++ NBSChat/NBSChat/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Hardcodet.Wpf.TaskbarNotification;
+
+
+namespace NBSChat
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static TaskbarIcon TaskIcon { get; set; }
+
+        //private WindowState _lastWinState = WindowState.Normal;//记录上一次窗口状态
+
+        //private bool _reallyExit = false;
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TaskIcon = new TaskbarIcon();
+
+        }
+
+
+        #region 窗体相关操作
+        /// <summary>
+        /// 拖动窗口
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        /// <summary>
+        /// 关闭窗口
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Btn_Close_Click(object sender,RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        /// <summary>
+        /// 最小化窗口
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Btn_Min_Click(object sender,RoutedEventArgs e)
+        {
+            this.WindowState = System.Windows.WindowState.Minimized;
+            this.Visibility = Visibility.Hidden;
+            
+        }
+        #endregion
+
+        private void TbIcon_TrayLeftMouseDown(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ NBSChat/NBSChat/ViewModel/WinViewModelBase.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+/**
+ * ┌───────────────────────────────────────────────────────────────────────┐
+ * │Project	: NBSChat.ViewModel
+ * │ 
+ * │Comment	:
+ * │
+ * │Version	: V1.0.0.0
+ * │Author	    : lanbery
+ * │CreatTime	: 2019/2/26 14:17:06													
+ * ├───────────────────────────────────────────────────────────────────────┤
+ * │Copyright © NBS-Tech Team 2019.All rights reserved.
+ * └───────────────────────────────────────────────────────────────────────┘
+ */
+namespace NBSChat.ViewModel
+{
+    class WinViewModelBase : ViewModelBase
+    {
+        public WinViewModelBase(string windowName) : base()
+        {
+            this.WindowName = windowName;
+        }
+
+        #region 属性
+        private bool _isShow;
+        private string _title;
+
+        /// <summary>
+        /// 窗口注册的名称
+        /// </summary>
+        public string WindowName { get; private set; }
+
+        public Window Window { get; private set; }
+
+        public bool IsShow
+        {
+            get { return _isShow; }
+            set
+            {
+                if (_isShow != value)
+                {
+                    _isShow = value;
+                    this.RaisePropertyChanged("IsShow");
+                }
+            }
+        }
+
+        //<summary>
+        //窗体的窗体
+        //</summary>
+        public virtual string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (_title != value)
+                {
+                    _title = value;
+                    this.RaisePropertyChanged("Title");
+                }
+            }
+        }
+
+        #endregion
+
+        #region 命令
+        private RelayCommand _showCommand;
+        private RelayCommand _closeCommand;
+        //private RelayCommand _minimizeCommand;
+        //private RelayCommand _maximizeCommand;
+        //private RelayCommand _normalCommand;
+        //private RelayCommand _showDialogCommand;
+
+        ///<summary>打开命令</summary>
+        public RelayCommand ShowCommand
+        {
+            get
+            {
+                if (_showCommand == null)
+                {
+                    _showCommand = new RelayCommand(this.Show);
+                }
+                return _showCommand;
+            }
+        }
+        /// <summary>
+        /// 关闭命令
+        /// </summary>
+        public RelayCommand CloseCommand
+        {
+            get
+            {
+                if(_closeCommand == null)
+                {
+                    _closeCommand = new RelayCommand(this.Close);
+                }
+                return _closeCommand;
+            }
+        }
+        #endregion
+
+        #region 方法
+
+        ///<summary>显示窗口</summary>
+        public virtual void Show()
+        {
+            if (this.Window != null)
+            {
+                this.Window.Show();
+            } else
+            {
+                this.IsShow = true;
+            }
+        }
+
+        ///<summary>
+        ///关闭窗口
+        ///</summary>
+        public virtual void Close()
+        {
+            if(this.Window != null)
+            {
+                this.Window.Close();
+            }
+        }
+
+
+
+        #endregion
+    }
+
+
+
+}
