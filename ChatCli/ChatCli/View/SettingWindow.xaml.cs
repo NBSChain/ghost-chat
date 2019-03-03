1bbb6f822e23b93a801b3040f9c1620f93d0ec3f
lamborqq <471127079@qq.com>
2019-03-03 18:29:29 +0800

    ghost

---
+++ ChatCli/ChatCli/View/SettingWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+/**
+ * ┌───────────────────────────────────────────────────────────────────────┐
+ * │Project	: ChatCli.View
+ * │ 
+ * │Comment	:
+ * │
+ * │Version	: V1.0.0.0
+ * │Author	: lanbery
+ * │CreatTime	: 2019/3/3 17:01:51													
+ * ├───────────────────────────────────────────────────────────────────────┤
+ * │Copyright © NBS-Tech Team 2019.All rights reserved.
+ * └───────────────────────────────────────────────────────────────────────┘
+ */
+namespace ChatCli.View
+{
+    /// <summary>
+    /// SettingWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class SettingWindow : Window
+    {
+        public SettingWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Close_Click(object sender,RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ DemoTheme/DemoTheme/PageView/TestWinDemo.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using DMSkin.WPF;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+/**
+ * ┌───────────────────────────────────────────────────────────────────────┐
+ * │Project	: DemoTheme.PageView
+ * │ 
+ * │Comment	:
+ * │
+ * │Version	: V1.0.0.0
+ * │Author	: lanbery
+ * │CreatTime	: 2019/2/28 15:28:12													
+ * ├───────────────────────────────────────────────────────────────────────┤
+ * │Copyright © NBS-Tech Team 2019.All rights reserved.
+ * └───────────────────────────────────────────────────────────────────────┘
+ */
+namespace DemoTheme.PageView
+{
+    /// <summary>
+    /// TestWinDemo.xaml 的交互逻辑
+    /// </summary>
+    public partial class TestWinDemo : DMSkinSimpleWindow
+    {
+        public TestWinDemo()
+        {
+            InitializeComponent();
+        }
+    }
+}
