2bd6e5be1cf612af5059d35127fe9502c007851e
lamborqq <471127079@qq.com>
2019-03-11 18:47:36 +0800

    nbs

--- TerzoChat/TerzoChat/MainWindow.xaml.cs
+++ TerzoChat/TerzoChat/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 using TerzoChat.View;
 
 namespace TerzoChat
@@ -23,8 +25,34 @@ namespace TerzoChat
     {
         public MainWindow()
         {
-            
-            
+            bool b = CheckedNBSRunning();
+           
+            if (!b)
+            {
+                //MessageBox.Show("NBS 未启动");
+                string startPath = Directory.GetCurrentDirectory();
+                Console.WriteLine("====================>>>"+startPath);
+                try
+                {
+                    Process nbsp = new Process();
+                    nbsp.StartInfo.UseShellExecute = false;
+                    nbsp.StartInfo.FileName = startPath + System.IO.Path.DirectorySeparatorChar + "nbs.exe";
+                    nbsp.StartInfo.CreateNoWindow = true;
+
+                    bool r = nbsp.Start();
+                    Console.WriteLine("启动》》" + r.ToString());
+                   
+
+                }catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("==================》》NBS 已启动." );
+            }
             InitializeComponent();
             this.LoadChat();
         }
@@ -33,6 +61,23 @@ namespace TerzoChat
         {
             this.RChatContainer.Children.Clear();
             this.RChatContainer.Children.Add(new ChatMainView());
+        }
+
+        private bool CheckedNBSRunning()
+        {
+            bool b = false;
+            Process[] processes;
+            try
+            {
+                processes = Process.GetProcessesByName("nbs");
+                Console.WriteLine("nbs process :" + processes.Length.ToString());
+                return processes.Length > 0;
+                
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
     }
 }
